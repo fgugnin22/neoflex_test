afa89b6da3e0f433e91656b467aa311d42486ade
fgugnin22 <fedorgugnin22@gmail.com>
2024-03-17 00:08:39 +0300

    added custom shadow to tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
       colors: { price: "#FFA542", main: "#1C1C27", secondary: "#838383", dark: "#101010" },
       fontFamily: {
         'sans': "Montserrat, Arial, 'sans-serif'"
+      },
+      boxShadow: {
+        main: '0px 0px 20px rgba(0, 0, 0, 0.15)'
       }
     },
   },
